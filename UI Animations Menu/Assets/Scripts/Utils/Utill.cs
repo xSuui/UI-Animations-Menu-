da18e16048a84d12b79b6ab8edbfc886e9901ed4
xSuui <samuel2012rogerio@gmail.com>
2023-11-08 23:25:07 -0300

    add menu to toolbar

--- UI Animations Menu/Assets/Scripts/Utils/Utill.cs
+++ UI Animations Menu/Assets/Scripts/Utils/Utill.cs
@@ -4,9 +4,43 @@ using System.Collections.Generic;
 using UnityEngine;
 using Random = UnityEngine.Random;
 using DG.Tweening;
+//using UnityEditor;
 
 public static class Utill 
 {
+#if UNITY_EDITOR    
+    [UnityEditor.MenuItem("Ebac/Test")]
+    public static void Test()
+    {
+        Debug.Log("Test");
+    }
+    
+    [UnityEditor.MenuItem("Ebac/Test2")]
+    public static void Test2()
+    {
+        Debug.Log("Test2");
+    }
+    
+    /*[UnityEditor.MenuItem("Ebac/Create GameObject %g")]
+    public static void CreateGameObject()
+    {
+        GameObject newObject = new GameObject("MeuNovoGameObject"); // Crie um novo GameObject com um nome personalizado
+                                                                    // Você pode adicionar mais personalizações ao novo GameObject aqui, se necessário
+        Debug.Log("Novo GameObject criado: " + newObject.name);
+    }*/
+
+    [UnityEditor.MenuItem("Ebac/Create GameObject %g")]
+    public static void CreateGameObject()
+    {
+        GameObject newObject = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        newObject.name = "MinhaNovaEsfera"; // Nome personalizado
+                                            // Você pode adicionar mais personalizações ao novo GameObject aqui, se necessário
+        Debug.Log("Nova esfera criada: " + newObject.name);
+    }
+
+#endif
+
+
 
     public static void Scale(this Transform t, float size = 1.2f)
         {
